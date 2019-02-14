f698ba937500257737c8e3dbfd540c2fc2aaab75
nokhodian <nokhodian@gmail.com>
2019-02-14 23:16:30 +0100

    some formatting

--- index.js
+++ index.js
@@ -56,10 +56,68 @@ app.post("/exceltojson", upload.single("file"), function (req, res) {
       '*': '{{columnHeader}}'
   }
 });
+var respond = {
+  "meta": {
+  "id": 1
+  },
+  "outfits": []
+}
+  
+result['Sheet1'].forEach(element => {
+  var tempElement = {};
 
-res.send(result)
-  return new Promise(function (resolve, reject) {
-    console.log(req.file);
-  });
+  tempElement["id"]= element["Outfit ID"]
+  tempElement["gender"]= element["Gender"]
+  tempElement["title"]= element["Title"]
+  tempElement["description"]= ""
+  //tempElement["Image_url"]= element["Image URL"]
+  tempElement["Image_url"]= element["Pic Link"]
+  
+  tempElement["influencer"] = {}
+
+  tempElement["influencer"]["id"]= element["Influencer ID"]
+  
+  tempElement["influencer"]["name"]= element["Name"]
+  tempElement["influencer"]["country"]= element["Country"]
+  tempElement["influencer"]["description"]= ""
+  tempElement["influencer"]["Profile_image_url"]= element["Profile Image URL"]
+  tempElement["influencer"]["header_image_url"]= ""
+
+  
+  tempElement["influencer"]["social_accounts"]= {}
+  tempElement["influencer"]["social_accounts"]["instagram"] = element["Instagram URL"]
+  tempElement["influencer"]["social_accounts"]["facebook"] = ""
+  tempElement["influencer"]["social_accounts"]["pinterest"] = ""
+  tempElement["influencer"]["social_accounts"]["tumblr"] = ""
+  tempElement["influencer"]["social_accounts"]["web"] = ""
+  tempElement["Tags"]= []
+  if(element["Tag 1"])
+  tempElement["Tags"].push(element["Tag 1"])
+  if(element["Tag 2"])
+  tempElement["Tags"].push(element["Tag 2"])
+  if(element["Tag 3"])
+  tempElement["Tags"].push(element["Tag 3"])
+
+  tempElement["articles"]= []
+  if(element["SKU 1"])
+  tempElement["articles"].push({"match_type": "exact","sku":element["SKU 1"]})
+  if(element["SKU 2"])
+  tempElement["articles"].push({"match_type": "exact","sku":element["SKU 2"]})
+  if(element["SKU 3"])
+  tempElement["articles"].push({"match_type": "exact","sku":element["SKU 3"]})
+  if(element["SKU 4"])
+  tempElement["articles"].push({"match_type": "exact","sku":element["SKU 4"]})
+  if(element["SKU 5"])
+  tempElement["articles"].push({"match_type": "exact","sku":element["SKU 5"]})
+  if(element["SKU 6"])
+  tempElement["articles"].push({"match_type": "exact","sku":element["SKU 6"]})
+
+
+
+  respond.outfits.push(tempElement);
+
+});
+respond.outfits.shift();
+res.send(respond)
 });
 
