7f34644d7aaf2dd6bb81f60e8846cb44c26b64a1
nokhodian <nokhodian@gmail.com>
2019-02-27 04:02:06 +0100

    create html

--- index.js
+++ index.js
@@ -43,9 +43,15 @@ var storage = multer.diskStorage({
 var upload = multer({
   storage: storage
 });
+var respond = {
+  "meta": {
+    "id":  1
+  },
+  "outfits": []
+}
 
 //~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.
-
+var html = "";
 app.post("/exceltojson", upload.single("file"), function (req, res) {
   var response = {};
 
@@ -58,17 +64,12 @@ app.post("/exceltojson", upload.single("file"), function (req, res) {
   });
 
   var pagenum = parseInt(req.body.page);
-  var respond = {
-    "meta": {
-      "id": pagenum + 1
-    },
-    "outfits": []
-  }
+  
 
-
-  var counter = pagenum * 151;
-  var last = counter + 151;
+  var counter = 0;
+  var last = 310;
   var count = 0
+
   console.log("products from " + counter + " to " + last);
 
   result['Sheet1'].forEach(element => {
@@ -81,7 +82,7 @@ app.post("/exceltojson", upload.single("file"), function (req, res) {
       tempElement["gender"] = element["Gender"]
       tempElement["title"] = element["Title"]
       tempElement["description"] = ""
-      tempElement["image_url"]= element["Image URL"]
+      tempElement["image_url"] = element["Image URL"]
       checkUrl(element["Image URL"])
 
 
@@ -97,7 +98,7 @@ app.post("/exceltojson", upload.single("file"), function (req, res) {
       tempElement["influencer"]["profile_image_url"] = element["Profile Image URL"]
       tempElement["influencer"]["header_image_url"] = ""
       checkUrl(element["Profile Image URL"])
-      
+
 
       tempElement["influencer"]["social_accounts"] = {}
       tempElement["influencer"]["social_accounts"]["instagram"] = element["Instagram URL"]
@@ -114,48 +115,58 @@ app.post("/exceltojson", upload.single("file"), function (req, res) {
         tempElement["Tags"].push(element["Tag 3"])
 
       tempElement["articles"] = []
-      if (element["SKU 1"])
+      if (element["SKU 1"]) {
         tempElement["articles"].push({
           "match_type": "similar",
           "sku": element["SKU 1"]
         })
-      if (element["SKU 2"])
+
+      }
+      if (element["SKU 2"]) {
         tempElement["articles"].push({
-          "match_type": "exact",
+          "match_type": "similar",
           "sku": element["SKU 2"]
         })
-      if (element["SKU 3"])
+      }
+      if (element["SKU 3"]) {
         tempElement["articles"].push({
-          "match_type": "exact",
+          "match_type": "similar",
           "sku": element["SKU 3"]
         })
-      if (element["SKU 4"])
+      }
+      if (element["SKU 4"]) {
         tempElement["articles"].push({
-          "match_type": "exact",
+          "match_type": "similar",
           "sku": element["SKU 4"]
         })
-      if (element["SKU 5"])
+      }
+      if (element["SKU 5"]) {
         tempElement["articles"].push({
-          "match_type": "exact",
+          "match_type": "similar",
           "sku": element["SKU 5"]
         })
-      if (element["SKU 6"])
+      }
+      if (element["SKU 6"]) {
         tempElement["articles"].push({
-          "match_type": "exact",
+          "match_type": "similar",
           "sku": element["SKU 6"]
         })
+      }
 
-      if(element["SKU 1"]){
-      respond.outfits.push(tempElement);
+      //double check if sku 1 doesnt exist dont add it
+      if (element["SKU 1"]) {
+        respond.outfits.push(tempElement);
         counter++;
-    }
+      }
+
     }
   });
   console.log(count + " Items was read");
 
 
   respond.outfits.shift();
-
+  links.shift();
+  links.shift();
 
   var filename = req.file.filename;
   filename = filename.replace(/\..+$/, '');
@@ -167,6 +178,8 @@ app.post("/exceltojson", upload.single("file"), function (req, res) {
     }
   });
 
+  //starttocheck(links.pop());
+  starttocreatehtml(0);
   res.send(respond)
 });
 
@@ -181,17 +194,135 @@ app.get("/product/:id", function (req, res) {
 
 });
 
+var links = []
 
-function checkUrl(url){
-  request.head(url, function (error, response) {
-    try{
-    if(response.statusCode != 200){
-    console.log('url:', url);
-    console.log('statusCode:', response.statusCode); 
-    }
-  }catch(e){
-    console.log('problem: '+e);
-  console.log('url:', url);
+function checkUrl(url) {
+  links.push(url);
 }
+var countFine = 0
+
+function starttocheck(url) {
+  request.head(url, function (error, response) {
+    try {
+      if (response.statusCode != 200) {
+        console.log('url:', url);
+        console.log('statusCode:', response.statusCode);
+      } else {
+        countFine++;
+      };
+      var newurl = links.pop();
+      if (newurl)
+        starttocheck(newurl)
+      else
+        console.log(countFine + " Images are fine");
+
+      console.log(countFine + " Images are fine");
+    } catch (e) {
+      console.log('problem: ' + e);
+      console.log('url:', url);
+    }
   });
+}
+
+var count = 0;
+var html = ""
+async function starttocreatehtml(num) {
+console.log("start to create "+ num);
+  var filename = publicDir + '/somerandom.html';
+
+  var url = "https://www.zalando.de/katalog/?q="
+  var localhtml="";
+try{
+  localhtml+= "<p><p><p> id: "+respond.outfits[num].id+"<p>"
+  localhtml+="<img src="+respond.outfits[num].image_url+"><p>"
+ 
+    if(respond.outfits[num].articles[0])
+    request.get(url+respond.outfits[num].articles[0].sku, function (error, response) {
+      var substring = response.body.substr(response.body.search('og:image'), 240);
+      var substring = substring.match('https.*\.jpg');
+     
+      if(substring)
+      localhtml+="<img alt='' src="+substring[0]+" id=''><p>"
+       
+       console.log("1 done");
+
+       if(respond.outfits[num].articles[1])
+       request.get(url+respond.outfits[num].articles[1].sku, function (error, response) {
+        var substring = response.body.substr(response.body.search('og:image'), 240);
+        var substring = substring.match('https.*\.jpg');
+      
+       if(substring)
+       localhtml+="<img alt='' src="+substring[0]+" id=''><p>"
+        
+        console.log("2 done");
+        console.log(url+respond.outfits[num].articles[2].sku);
+
+        if(respond.outfits[num].articles[2])
+       request.get(url+respond.outfits[num].articles[2].sku, function (error, response) {
+    
+        var substring = response.body.substr(response.body.search('og:image'), 240);
+        var substring = substring.match('https.*\.jpg');
+        
+       if(substring)
+       localhtml+="<img alt='' src="+substring[0]+" id=''><p>"
+        console.log("3 done");
+
+
+        if(respond.outfits[num].articles[3]){
+       request.get(url+respond.outfits[num].articles[3].sku, function (error, response) {
+        var substring = response.body.substr(response.body.search('og:image'), 240);
+        var substring = substring.match('https.*\.jpg');
+      
+       if(substring)
+        localhtml+="<img alt='' src="+substring[0]+" id=''><p>"
+        console.log("4 done");
+
+
+        if(respond.outfits[num].articles[4]){
+       request.get(url+respond.outfits[num].articles[4].sku, function (error, response) {
+        var substring = response.body.substr(response.body.search('og:image'), 240);
+        var substring = substring.match('https.*\.jpg');
+      
+       if(substring)
+       localhtml+="<img alt='' src="+substring[0]+" id=''><p>"
+        console.log("5 done");
+        console.log("5 done");
+
+
+        if(respond.outfits[num].articles[5]){
+       request.get(url+respond.outfits[num].articles[5].sku, function (error, response) {
+        var substring = response.body.substr(response.body.search('og:image'), 240);
+        var substring = substring.match('https.*\.jpg');
+      
+       if(substring)
+       localhtml+="<img alt='' src="+substring[0]+" id=''><p>"
+        console.log("6 done");
+
+        fs.appendFile(filename, localhtml, function (err) {
+          if(num <302)
+          starttocreatehtml(num+1)
+        });
+
+    });}else{fs.appendFile(filename, localhtml, function (err) {
+      if(num <302)
+      starttocreatehtml(num+1)
+    });}
+  });}else{fs.appendFile(filename, localhtml, function (err) {
+    if(num <302)
+    starttocreatehtml(num+1)
+  });}
+});}else {fs.appendFile(filename, localhtml, function (err) {
+  if(num <302)
+  starttocreatehtml(num+1)
+});}
+  });});});
+
+
+    } catch (e) {
+      console.log('problem: ' + e);
+    }
+}
+
+function savefile(localhtml){
+  
 }
